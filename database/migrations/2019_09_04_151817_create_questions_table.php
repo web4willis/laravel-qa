c6993aa69465afcd29b559954e9611b7dac8f095
contact@web4infinity.com <contact@web4infinity.com>
2019-09-04 17:53:43 +0100

    Seed users and q

--- database/migrations/2019_09_04_151817_create_questions_table.php
+++ database/migrations/2019_09_04_151817_create_questions_table.php
@@ -16,7 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->string('slag')->unique();
+            $table->string('slug')->unique();
             $table->text('body');
             $table->unsignedInteger('views')->default(0);
             $table->unsignedInteger('answers')->default(0);
